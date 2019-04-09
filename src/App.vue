c741b6d60434d481f511ed6c5613642f6371a7ec
rinziv <rinzivillo@isti.cnr.it>
2019-04-09 17:29:36 +0200

    configuring NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <div id="app">
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">VC 2008</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Mini Challenge 1</b-nav-item>
+          <b-nav-item href="#">Mini Challenge 2</b-nav-item>
+          <b-nav-item href="#">Mini Challenge 3</b-nav-item>
+          <b-nav-item href="#">Mini Challenge 4</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
   </div>
 </template>
 
@@ -19,6 +33,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
