33a0d84b1c4f8187553dd15651b07b178571282a
rinziv <rinzivillo@isti.cnr.it>
2019-04-11 22:50:54 +0200

    Introducing map visualization

---
+++ src/components/MC2Map.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg height="500" width="100%">
+    <g class="world" ref="world"></g>
+    <g class="reports" ref="reports"></g>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'MC2Map',
+};
+</script>
+
