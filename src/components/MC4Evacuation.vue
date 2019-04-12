f4cec50803695e260432c53f9793824c8d491c58
rinziv <rinzivillo@isti.cnr.it>
2019-04-13 00:28:25 +0200

    Import data files. Loading building map

--- src/components/MC4Evacuation.vue
+++ src/components/MC4Evacuation.vue
@@ -6,7 +6,7 @@
     <b-row>
       <b-col>
         <h2>Building Map</h2>
-        <div style="height:500px; background-color: lightyellow"></div>
+        <svg width="100%" height="500"></svg>
       </b-col>
       <b-col cols="3">
         <h2>Persons</h2>
@@ -19,6 +19,14 @@
 <script>
 export default {
   name: 'MC4Evacuation',
+  mounted() {
+    fetch('/static/data/building_map.txt')
+      .then(response => response.text())
+      .then((map) => {
+        const aMap = map.split('\n').map(d => d.trim().split(' ').map(e => +e));
+        console.log('map', aMap);
+      });
+  },
 };
 </script>
 
