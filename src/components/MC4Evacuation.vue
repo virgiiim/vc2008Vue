010a225673c6e88a6677cef4888cdd9a33ed74a7
rinziv <rinzivillo@isti.cnr.it>
2019-04-13 00:38:55 +0200

    Drawing the map building

--- src/components/MC4Evacuation.vue
+++ src/components/MC4Evacuation.vue
@@ -6,9 +6,11 @@
     <b-row>
       <b-col>
         <h2>Building Map</h2>
-        <svg width="100%" height="500"></svg>
+        <svg width="100%" height="500">
+          <g class="building" ref="building"></g>
+        </svg>
       </b-col>
-      <b-col cols="3">
+      <b-col cols="2">
         <h2>Persons</h2>
         <div style="height:500px; background-color: lightyellow"></div>
       </b-col>
@@ -18,7 +20,7 @@
 
 <script>
 import BuildingBitmap from '@/assets/BuildingBitmap';
-
+const d3 = require('d3');
 export default {
   name: 'MC4Evacuation',
   mounted() {
@@ -27,6 +29,11 @@ export default {
       .then((map) => {
         const aMap = map.split('\n').map(d => d.trim().split(' ').map(e => +e));
         console.log('map', aMap);
+
+        const bb = BuildingBitmap();
+        d3.select(this.$refs.building)
+          .datum(aMap)
+          .call(bb);
       });
   },
 };
