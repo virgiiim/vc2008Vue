5dbcbf62204c4fe552bd7c7cebc9e2f90480b380
rinziv <rinzivillo@isti.cnr.it>
2019-05-21 20:37:41 +0200

    Adjusting forces of the simulation

--- src/assets/BubbleChart.js
+++ src/assets/BubbleChart.js
@@ -4,21 +4,25 @@ export default function BubbleChart() {
   const field = 'num_call';
   const r = d3.scaleSqrt()
     .domain([0, 1000])
-    .range([0, 15]);
+    .range([0, 10]);
 
   const width = 500;
   const height = 500;
 
   const simulation = d3.forceSimulation()
     .force('charge', d3.forceManyBody().strength(-1))
+    .force('forceX', d3.forceX(width / 2).strength(0.004))
+    .force('forceY', d3.forceY(height / 2).strength(0.004))
     .force('center', d3.forceCenter(width / 2, height / 2))
     .force('collide', d3.forceCollide().radius(d => r(d[field]) + 0.5).iterations(5));
 
   function me(selection) {
     console.log(selection);
-    simulation.nodes(selection.datum());
+
 
     r.domain([0, d3.max(selection.datum(), d => d[field])]);
+    simulation.nodes(selection.datum().sort((a,b) => b[field] - a[field]))
+      .force('collide', d3.forceCollide().radius(d => r(d[field]) + 0.5).iterations(5));
 
     const gCircle = selection.selectAll('g.circle')
       .data(selection.datum())
@@ -36,7 +40,7 @@ export default function BubbleChart() {
     simulation.on('tick', () => {
       gCircle
         .attr('transform', d => `translate(${d.x}, ${d.y})`);
-    })
+    });
   }
 
 
