95fefa56f7c6d8b65426cffc51c10b2a38580230
rinziv <rinzivillo@isti.cnr.it>
2019-05-21 13:50:58 +0200

    Introducing new component for MC3

---
+++ src/components/MC3PhoneCalls.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-container>
+    <b-row>
+      <h1>MC3 - Phone calls</h1>
+    </b-row>
+    <b-row>
+      <b-col>
+        <svg width="100%" height="500">
+          <g class="persons" red="persons"></g>
+        </svg>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'MC3PhoneCalls',
+  data() {
+    return {
+      calls: [],
+    };
+  },
+};
+</script>
+
